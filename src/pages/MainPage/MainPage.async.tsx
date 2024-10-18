a45fd8ff7588a8a087ad0932e783884a6c566a24
danilov <danilov@smartfarm.vision>
2024-10-18 14:17:17 +0300

    initial commit

---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,9 @@
+import { lazy } from 'react'
+
+export const MainPageAsync = lazy(
+	() =>
+		new Promise((resolve, reject) => {
+			// @ts-ignore
+			setTimeout(() => resolve(import('./MainPage')), 1500)
+		})
+)
