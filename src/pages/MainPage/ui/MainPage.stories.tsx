d90e6a5b86557aaac83d1eeaffc45a4554719dcb
klipperklip <klipper.klip@gmail.com>
2024-10-20 20:21:54 +0300

    fix args in page without props

--- src/pages/MainPage/ui/MainPage.stories.tsx
+++ src/pages/MainPage/ui/MainPage.stories.tsx
@@ -13,11 +13,9 @@ export default {
 
 } as ComponentMeta<typeof MainPage>;
 
-const Template: ComponentStory<typeof MainPage> = (args) => <MainPage {...args} />;
+const Template: ComponentStory<typeof MainPage> = () => <MainPage />;
 
 export const Normal = Template.bind({});
-Normal.args = {};
 
 export const Dark = Template.bind({});
-Dark.args = {};
 Dark.decorators = [ThemeDecorator(Theme.DARK)];
